88349dd0abe91636a9b0a7d15880908cf8c70251
sebastiano minotti <seba.minotti@gmail.com>
2021-04-26 10:55:13 +0200

    quarto push
    miglioria e correzione bug prezzo

--- main.js
+++ main.js
@@ -12,10 +12,10 @@
 var passeggero = prompt("SALVE PASSEGGERO , INSERISCI NOME E COGNOME");
 document.getElementById("passeggero").innerHTML=passeggero;
 
-var eta = parseInt(prompt("ORA INSERISCI LA TUA ETA' GRAZIE"));
+var eta = prompt("ORA INSERISCI LA TUA ETA' GRAZIE");
 document.getElementById("eta").innerHTML= "LA TUA ETA' " + eta;
 
-var km = parseInt(prompt("ORA DIMMI : QUANTA STRADA VORRESTI PERCORRERE ?"));
+var km = prompt("ORA DIMMI : QUANTA STRADA VORRESTI PERCORRERE ?");
 document.getElementById("km").innerHTML="KM PERCORSI : " + km 
 const prezzoKmIntero = 0.21 ;
 var totale ;
@@ -23,13 +23,13 @@ var totale ;
 
 // applichiamo uno sconto del 20% al prezzo di 0.21 che corrisponde allo sconto giovani
 var scontoGiovani = 0.21 / 100 * 80;
-scontoGiovani = scontoGiovani.toFixed(3);
+
 
 
 
 // applichiamo uno sconto del 40% al prezzo di 0.21 che corrisponde allo sconto anziani
 var scontoAnziani = 0.21 / 100 * 60;
-scontoAnziani = scontoAnziani.toFixed(3)
+
 
 
 
@@ -57,18 +57,21 @@ if (eta > 99) {
 
 if (eta < 18) {
     totale = km * scontoGiovani ;
+    totale = totale.toFixed(2)
     document.getElementById("biglietto").innerHTML= "Il totale è di: " + totale + " €"
     document.getElementById("sconto").innerHTML= " LO SCONTO APPLICATO è DEL 20% (SCONTO UTENTI JUNIOR)"
 }
  
 else if (eta >65){
     totale = km * scontoAnziani
+    totale = totale.toFixed(2)
     document.getElementById("biglietto").innerHTML= "Il totale è di: " +  totale  + " €"
     document.getElementById("sconto").innerHTML= " LO SCONTO APPLICATO è DEL 40% (SCONTO UTENTI SENIOR)"
 } 
 
 else if (eta > 17 && eta <65) {
     totale = km * prezzoKmIntero
+    totale = totale.toFixed(2)
     document.getElementById("biglietto").innerHTML= "Il totale è di: " + totale + " €"
     document.getElementById("sconto").innerHTML= " PREZZO INTERO (NESSUNO SCONTO APPLICABILE)"
 }
